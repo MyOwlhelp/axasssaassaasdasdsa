862c1a0a083f8b0cbd27b1e7ceddcf77c066ab8f
MyOwlhelp <thatlilguy23@gmail.com>
2025-02-18 18:55:15 +0700

    Update reader.lua

--- reader.lua
+++ reader.lua
@@ -1,11 +1,12 @@
 -- Not using slow metatables here because we need it fast
 local FLOAT_PRECISION = 24
+
 local Reader = {}
 
 function Reader.new(bytecode)
 	local stream = buffer.fromstring(bytecode)
-	local cursor = 1  -- Start from index 1 (Lua convention)
-
+	local cursor = 0
+	--
 	local self = {}
 
 	function self:len()
@@ -14,12 +15,12 @@ function Reader.new(bytecode)
 
 	function self:nextByte()
 		local result = buffer.readu8(stream, cursor)
-		cursor = cursor + 1
+		cursor += 1
 		return result
 	end
 	function self:nextSignedByte()
 		local result = buffer.readi8(stream, cursor)
-		cursor = cursor + 1
+		cursor += 1
 		return result
 	end
 	function self:nextBytes(count)
@@ -37,30 +38,30 @@ function Reader.new(bytecode)
 
 	function self:nextUInt32()
 		local result = buffer.readu32(stream, cursor)
-		cursor = cursor + 4
+		cursor += 4
 		return result
 	end
 	function self:nextInt32()
 		local result = buffer.readi32(stream, cursor)
-		cursor = cursor + 4
+		cursor += 4
 		return result
 	end
 
 	function self:nextFloat()
 		local result = buffer.readf32(stream, cursor)
-		cursor = cursor + 4
+		cursor += 4
 		return tonumber(string.format(`%0.{FLOAT_PRECISION}f`, result))
 	end
 
 	function self:nextVarInt()
 		local result = 0
-		local shift = 0
-		local byte
-		repeat
-			byte = self:nextByte()
-			result = result + ((byte % 128) * (2 ^ shift)) -- Simulate bitwise OR and left shift
-			shift = shift + 7
-		until (byte < 128) or (shift >= 32) -- Check if the MSB is 0 or shift exceeds 32 bits
+		for i = 0, 4 do
+			local b = self:nextByte()
+			result = bit32.bor(result, bit32.lshift(bit32.band(b, 0x7F), i * 7))
+			if not bit32.btest(b, 0x80) then
+				break
+			end
+		end
 		return result
 	end
 
@@ -70,22 +71,22 @@ function Reader.new(bytecode)
 			return ""
 		else
 			local result = buffer.readstring(stream, cursor, len)
-			cursor = cursor + len
+			cursor += len
 			return result
 		end
 	end
 
 	function self:nextDouble()
 		local result = buffer.readf64(stream, cursor)
-		cursor = cursor + 8
+		cursor += 8
 		return result
 	end
 
 	return self
 end
 
-function Reader:Set(precision)
-	FLOAT_PRECISION = precision or 24  -- Default value
+function Reader:Set(...)
+	FLOAT_PRECISION = ...
 end
 
 return Reader
